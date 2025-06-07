734f5fa7999040fd917d7d7e2a9df722e6933e3e
Frajam75 <francesca.cervellone@gmail.com>
2025-06-07 11:00:43 +0200

    Add files via upload

---
+++ salva_visibilita.js
@@ -0,0 +1,26 @@
+
+document.getElementById('visibilityForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(this);
+    const visibilitySettings = {};
+    formData.forEach((value, key) => {
+        visibilitySettings[key] = true;
+    });
+
+    const tuttiIModuli = ['ascolto', 'karaoke', 'chat', 'voti', 'suggerimenti', 'annunci', 'pagina_visibile'];
+    tuttiIModuli.forEach(modulo => {
+        if (!visibilitySettings[modulo]) {
+            visibilitySettings[modulo] = false;
+        }
+    });
+
+    fetch('https://script.google.com/macros/s/AKfycbyT5LPQryhmB_ZknfXPM3NUxlm3yb9m1g08nNmDUGSGRzx-D17UEiWwYG-urPNgYfkqMg/exec', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify({ tipo: 'visibilita', dati: visibilitySettings })
+    }).then(() => {
+        document.getElementById('salvataggioStatus').innerText = "Impostazioni salvate con successo!";
+    }).catch(() => {
+        document.getElementById('salvataggioStatus').innerText = "Errore nel salvataggio.";
+    });
+});
