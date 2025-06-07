c82cc51d3bd448d01629f8ea08423965b55d3ec5
Frajam75 <francesca.cervellone@gmail.com>
2025-06-07 13:40:51 +0200

    Add files via upload

--- salva_visibilita.js
+++ salva_visibilita.js
@@ -16,6 +16,7 @@ document.getElementById('visibilityForm').addEventListener('submit', function(e)
 
     fetch('https://script.google.com/macros/s/AKfycbyT5LPQryhmB_ZknfXPM3NUxlm3yb9m1g08nNmDUGSGRzx-D17UEiWwYG-urPNgYfkqMg/exec', {
         method: 'POST',
+        mode: 'no-cors',
         body: JSON.stringify({ tipo: 'visibilita', dati: visibilitySettings })
     }).then(() => {
         document.getElementById('salvataggioStatus').innerText = "Impostazioni salvate con successo!";
